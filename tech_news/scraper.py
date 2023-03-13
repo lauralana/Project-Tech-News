9d9f0f47d3fd37791d86c0fc04354809ad6e652e
Laura Lana <lauramarqueslana@gmail.com>
2023-03-13 17:27:05 -0300

    req 02

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import time
 import requests
+from parsel import Selector
 
 
 # Requisito 1
@@ -16,7 +17,8 @@ def fetch(url):
 
 # Requisito 2
 def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return selector.css('.cs-overlay-link::attr(href)').getall()
 
 
 # Requisito 3
